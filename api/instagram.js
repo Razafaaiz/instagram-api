06d0d8729dd68c43c22486b2205a1e8ef6b6b6aa
ultimezfaiz <ultimezfaiz@gmail.com>
2026-03-23 11:35:43 +0530

    update

--- api/instagram.js
+++ api/instagram.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const ACCESS_TOKEN = "EAAZA5BhlcMKsBREycQbub98WgYuioLZC4muOM2ZCZBpEbkZAF6WROpxhAQlzJtjT8QEGurnDu3eXdTOXx6aROV0NP9weOzZCNTN2gwZBZBo2cZCAgEZAyvUurjvJhWKP6eXPiy8dCQuoMRSPEM22rTMy2sIbLHW0IHTIUahZAqL6iU4fpJKZBNKshfJ1IDqGeUZB8lz053Tz81NXVNr4NkHSShC2sLZCZA95H2FxLCVNZBgVQVzSZCzeZBC5r3SfY3SrWmVQHhiZAgWdZA1LzLx2MzZAdtAihC0vTMRic";
+  const ACCESS_TOKEN = "EAAZA5BhlcMKsBRPoJa3atqX4ZAoskRAjn600RqbVWEpckeSaNBcXMIruJvhSXRWi3PXXuFrwIJyT63lEYNuJtijUSD9af7MMfiGGy7JSFADHFOzHfhVUn3XdReBK8JbufsZBgqdsdSjHTtadZAywsKrZCrdnw6XZA0LGNZBTY98oG3ZBgYxTAMyHOHRCZC64ZCFHO5nQ1zHFW4RCy1PySn9OwWoCCT8Vlif0kuVdtWFkvkFJUyYv3SzTnO5Bzg8ErHG6NKNgwrVNwtsWQzu3gPlQU2gHZBE";
   const IG_USER_ID = "17841441280435160";
 
   const url = `https://graph.facebook.com/v18.0/${IG_USER_ID}/media?fields=id,caption,media_type,permalink,like_count,comments_count&access_token=${ACCESS_TOKEN}`;
